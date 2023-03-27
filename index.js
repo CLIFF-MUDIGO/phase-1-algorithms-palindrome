fd47a4c0f12dc8194bd7bc10fd189368cae5517c
cliff <cliffmudigo5@gmail.com>
2023-03-27 22:39:19 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function isPalindrome(word) {
+  var re = /[\W_]/g;
+  var lowRegStr = word.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
   // Write your algorithm here
 }
 
